d394177343372d3bafdfcc5559826c8bd7d6b6b0
kingtamaaj <kingtamaaj@gmail.com>
2019-04-11 19:15:02 -0400

    Started on Person and Instructor Class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,34 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(name, age, location, gender) {
+    this.name = name;
+    this.age = age;
+    this.location = location;
+    this.gender = gender;
+    }
+
+    speak() {
+        return `{Hello my name is ${this.name}, I am from ${this.location}.}`;
+    }
+}
+
+class Instructor {
+    constructor(name, age, location, gender, specialty, favLanguage, catchPhrase) {
+    this.name = name;
+    this.age = age;
+    this.location = location;
+    this.gender = gender;
+    this.specialty = specialty;
+    this.favLanguage = favLanguage;
+    this.catchPhrase = catchPhrase;
+    }
+
+    demo(subject) {
+        return `{Today we are learning about ${this.subject}.}`;
+    }
+    grade(student, subject){
+        return `{$student.name} receives a perfect score on {$subject}`;
+    }
+}
+
+console.log(new Instructor())
